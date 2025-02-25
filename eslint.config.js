ddb4310eea5b2b16c83e70149fbc4525397ce714
Rajkishore Behera <rajkishorebehera358@gmail.com>
2025-02-25 21:24:21 +0530

    Fix ESLint config issue

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,9 @@
 export default [
-    {
-      ignores: ["node_modules/", "dist/"], // Ignore unnecessary files
-    },
-    {
-      files: ["**/*.js", "**/*.ts"],
-      rules: {
-        "no-unused-vars": "warn",
-        "no-console": "off"
-      }
+  {
+    files: ["**/*.js"],
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off"
     }
-  ];
-  
+  }
+];
